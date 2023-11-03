2b3bc92559351225aeb2d14727ea259058329d28
root <jennamar828@gmail.com>
2023-11-03 12:29:48 +0000

    delete function working

--- src/main.py
+++ src/main.py
@@ -15,14 +15,20 @@ select_all_heroes()
 # add a conditional to ask if the user wants to add a new hero
 # if the answer is "no" or "n" then the user should be able to skip this step 
 def add_hero():
-    name = input("Add your new hero name: ")
-    about = input("Tell me about your hero: ")
-    bio = input("Tell me about your hero's past: ")
-    query = """
-            INSERT INTO heroes (name, about_me, biography)
-            VALUES (%s, %s, %s);
-            """
-    execute_modify(query, (name, about, bio,))
+    prompt = input("Do you want to add a new hero? Y or N: ")
+    if prompt.upper() == "Y":
+        name = input("Add your new hero name: ")
+        about = input("Tell me about your hero: ")
+        bio = input("Tell me about your hero's past: ")
+        query = """
+                INSERT INTO heroes (name, about_me, biography)
+                VALUES (%s, %s, %s);
+                """
+        execute_modify(query, (name, about, bio,))
+    elif prompt.upper() == "N":
+        print("Add new hero skipped.")
+    else: 
+        print("Invalid entry. Please enter Y or N")
 
 add_hero()
 
@@ -33,15 +39,21 @@ add_hero()
 # add a conditional statement to ask if the user wants to update a hero
 # if "no" or "n" the user should be able to skip this step
 def update_hero():
-    name = input("Change the name of your hero: ")
-    about = input("Tell me about this new hero: ")
-    bio = input("Where did this new hero come from: ")
-    query = """
-            UPDATE heroes
-            SET name = (%s), about_me = (%s), biography = (%s)
-            WHERE name = @name;
-            """
-    execute_modify(query, (name, about, bio))
+    prompt = input("Do you want to update a hero? Y or N: ")
+    if prompt.upper() == "Y":
+        old_hero = input("Enter current hero name: ")
+        new_hero = input("What is the name of your new hero: ")
+        query = """
+                UPDATE heroes
+                SET name = (%s)
+                WHERE name = (%s);
+                """
+        execute_modify(query, (old_hero, new_hero))
+        print(old_hero, " changed to ", new_hero)
+    elif prompt.upper() == "N":
+        print("Skipped update")
+    else: 
+        print("invalid input. PLease enter Y or N")
 
 update_hero()
 
@@ -51,15 +63,17 @@ update_hero()
 # if "no" or "n" the user will skip this step
 def delete_hero():
     prompt = input("Are you sure you want to delete a hero? Y or N: ")
-    if prompt is "Y":
+    if prompt.upper() == "Y":
         name = input("what hero are you wanting to delete: ")
-    else: 
-        
-    query = """
+        query = """
             DELETE
             FROM heroes
             WHERE name = (%s);
             """
-    execute_modify(query, (prompt, name))
+        execute_modify(query, (name,))
+    elif prompt.upper() == "N":
+        print("Hero deleted")
+    else:
+        print("Invalid input. Please print Y or N")
 
 delete_hero()
--- src/pseudocode.md
+++ src/pseudocode.md
@@ -4,9 +4,9 @@
 
 2. The supplied superheroes.sql database file (attached to global classroom assignment) contains CREATE TABLE and INSERT statements to get you started with seeded data. (Do not modify the SQL file) Use the VSCode extension to execute the file from the top to bottom to get the appropriate tables/entries created. (complete)
 
-3. Decide/Plan/Pseudocode on a minimum of four CRUD operations you wish to implement (at least on operation for Create, Read, Update, & delete) - document these in your README.md
+3. Decide/Plan/Pseudocode on a minimum of four CRUD operations you wish to implement (at least on operation for Create, Read, Update, & delete) - document these in your README.md (complete)
 
-4. Interactive creation, update, delete of a hero in the terminal via Python script, with commands that are available for the README.md
+4. Interactive creation, update, delete of a hero in the terminal via Python script, with commands that are available for the README.md (complete?)
 
 - This is completely backend project. There will be no visual elements to it in a browser but will be interactive in the terminal
 
